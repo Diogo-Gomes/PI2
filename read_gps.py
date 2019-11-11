5c57a52b7bca5c95a2433f75b13b148cbd9e2d85
Diogo-Gomes <37155647+Diogo-Gomes@users.noreply.github.com>
2019-11-11 17:41:24 -0300

    Update read_gps.py

--- read_gps.py
+++ read_gps.py
@@ -1,6 +1,8 @@
+
 #!/usr/bin/env python
 import time
 import serial
+import sys
 
 ser = serial.Serial(
         port='/dev/serial0',
@@ -11,20 +13,27 @@ ser = serial.Serial(
         timeout=1
 )
 
-while 1:
-  line=str(ser.readline())
-  #print (line)
-  #for line in x:
-  if line.startswith("b'$GPRMC"):
-    line = line.strip()		
-    #print (line)
-    dados = line.split(',')
-    latitude = dados[3]
-    graus_lat = latitude[0]+latitude[1]
-    longitude = dados[5]
-    graus_long = longitude[0]+longitude[1]+longitude[2]
-    coordenadas = graus_lat+'º'+latitude[2:]+"'"+ dados[4]+','+graus_long+'º'+longitude[3:]+"'"+dados[6]	
-    print(coordenadas)
-    arq = open("file.txt","a")
-    arq.write(coordenadas)
-    arq.close()
+i=0
+
+while (i==0):
+
+        line=str(ser.readline())
+        #print (line)
+        #for line in x:
+        if line.startswith("b'$GPRMC"):
+                line = line.strip()
+                #print (line)
+                dados = line.split(',')
+                latitude = dados[3]
+                graus_lat = latitude[0]+latitude[1]
+                longitude = dados[5]
+                graus_long = longitude[0]+longitude[1]+longitude[2]
+                coordenadas = graus_lat+'º'+latitude[2:]+"'"+ dados[4]+','+graus_long+'º'+longitude[3:]+"'"+dados[6]
+                print(coordenadas)
+
+                arq = open("file.txt","a")
+                arq.write(coordenadas)
+                arq.close()
+
+                ser.close()
+                i=i+1
